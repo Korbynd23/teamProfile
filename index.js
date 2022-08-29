150553b7c772ce88eb6d5bca17be2e38fae32c2a
kdicari24@gmail.com <kdicari24@gmail.com>
2022-08-28 20:31:10 -0400

    created new employees with prompt and pushed them into employee bucket array

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer")
-const fs = reuqire("fs")
+const fs = require("fs")
 
 const Employee = require("./classes/Employee.js")
 const Engineer = require("./classes/Engineer.js")
@@ -15,94 +15,96 @@ const init = () => {
         {
             type: "input",
             message: "Enter Manager name",
-            name: "managerName"  
+            name: "managerName"
 
         },
         {
             type: "input",
             message: "Enter ID",
-            name: "managerId"  
+            name: "managerId"
 
         },
         {
             type: "input",
             message: "Enter email",
-            name: "managerEmail" 
+            name: "managerEmail"
 
         },
         {
             type: "input",
             message: "Enter Manager office number",
-            name: "managerOfficeNum" 
+            name: "managerOfficeNum"
 
         },
-    ]) .then((managerData)=> {
-        const {managerName, managerId, managerEmail, managerOfficeNum} = managerData
+    ]).then((managerData) => {
+        const { managerName, managerId, managerEmail, managerOfficeNum } = managerData
         const manager = new Manager(managerName, managerId, managerEmail, managerOfficeNum)
-        // employeeBucket.push(manager)
-        console.log(employeeBucket)
+        employeeBucket.push(manager)
+        // console.log(manager)
         anotherEmployee()
     })
 }
 const anotherEmployee = () => {
     return inquirer.prompt([
         {
-            type: "choices",
+            type: "list",
             message: "Would you like to add an employee?",
-            name: "employeeAdd",
             choices: [
                 "Engineer",
                 "Intern",
                 "No, I'm finished"
-            ]  
-            
+            ],
+            name: "employeeAdd",
+
         }
-    ]) .then((addEmployeeChoice) => {
-            switch(addEmployeeChoice.employeeAdd) {
-                case 'Engineer':
-                    addEngineer()
-                    break
-                case 'Intern':
-                    addIntern()
-                    break
-                case "No, I'm finished":
-                    pushEmployee()
-                    console.log("Employee(s) created")
-                    
-            }
+    ]).then((newCase) => {
+        switch (newCase.employeeAdd) {
+            case "Engineer":
+                addEngineer()
+                break;
+            case "Intern":
+                addIntern()
+                break;
+            case "No, I'm finished":
+                pushNewEmployee()
+                console.log(employeeBucket)
+                console.log("Employee(s) created")
+        }
     })
 }
 
 const addEngineer = () => {
+    console.log("made it to engineer")
     return inquirer.prompt([
         {
             type: "input",
             message: "Enter engineers name",
-            name: "engineerName"  
+            name: "engineerName"
 
         },
         {
             type: "input",
             message: "Enter new engineer employee ID",
-            name: "engineerId"  
+            name: "engineerId"
 
         },
         {
             type: "input",
             message: "Enter new engineer email",
-            name: "engineerEmail" 
+            name: "engineerEmail"
 
         },
         {
             type: "input",
             message: "Enter engineers GitHub username",
-            name: "engineerGit" 
+            name: "engineerGit"
 
         },
-    ]) .then((engineerData) => {
-        const {engineerName, engineerId, engineerEmail, engineerGit} = engineerData
+    ]).then((engineerData) => {
+        const { engineerName, engineerId, engineerEmail, engineerGit } = engineerData
         const engineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGit)
-        console.log(engineer)
+        employeeBucket.push(engineer)
+        // console.log(engineer)
         anotherEmployee()
     })
 }
@@ -112,40 +114,45 @@ const addIntern = () => {
         {
             type: "input",
             message: "Enter interns name",
-            name: "internName"  
+            name: "internName"
 
         },
         {
             type: "input",
             message: "Enter new intern employee ID",
-            name: "internId"  
+            name: "internId"
 
         },
         {
             type: "input",
             message: "Enter new intern email",
-            name: "internEmail" 
+            name: "internEmail"
 
         },
         {
             type: "input",
             message: "Name of school the new intern attends",
-            name: "internSchool" 
+            name: "internSchool"
 
         },
-    ]) .then((internData) =>{
-        const {internName, internId, internEmail, internSchool} = internData
+    ]).then((internData) => {
+        const { internName, internId, internEmail, internSchool } = internData
         const intern = new Intern(internName, internId, internEmail, internSchool)
-        console.log(intern)
+        // console.log(intern)
+        employeeBucket.push(intern)
         anotherEmployee()
 
     })
 }
 
-const fireHtml = () => {
-    //for each thing do card for string literals
+const pushNewEmployee = () => {
 
 }
+
+// // const fireHtml = () => {
+//     //for each thing do card for string literals
+
+// }
 
 //if else send to switch case engineer/ fire prompt. if finish fire generate html
 //three more function 1. intern 2.engineer 3.fire html
